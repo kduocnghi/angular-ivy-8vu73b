ad6fbfeebf6e52d95dcb265f55cf8e7841cc23d9
kduocnghi <kduocnghi@gmail.com>
2022-01-27 14:23:15 +0700

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,34 @@
+import { error } from '@angular/compiler/src/util';
 import { Component, VERSION } from '@angular/core';
+import { from, of } from 'rxjs';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
+export class AppComponent {
   name = 'Angular ' + VERSION.major;
+
+  postArray = [
+    { title: 'ducanh1', description: 'ducanh description 1' },
+    { title: 'ducanh2', description: 'ducanh description 2' },
+    { title: 'ducanh3', description: 'ducanh description 3' },
+  ];
+
+  postArrayObserverble$ = of(this.postArray);
+
+  constructor() {
+    this.postArrayObserverble$.subscribe({
+      next: (data) => {
+        console.log(data);
+      },
+      complete: () => {
+        console.log('complete');
+      },
+      error: (err) => {
+        console.log('error');
+      },
+    });
+  }
 }
